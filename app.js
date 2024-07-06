183d0a97a7f43681d3fa5e11489d0bd0439dc295
Ayush <168622573+ayush-uttam@users.noreply.github.com>
2024-07-06 13:14:43 +0530

    resumed music

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ let drawScore = document.querySelector("#drawScore");
 let result = document.querySelector("#result");
 let info = document.querySelector("#info");
 
-//let claps = document.querySelector("#claps");
-//let boo = document.querySelector("#boo");
-//let ohno = document.querySelector("#ohno");
+let claps = document.querySelector("#claps");
+let boo = document.querySelector("#boo");
+let ohno = document.querySelector("#ohno");
 
 let iterations = document.querySelector("#iterations");
 
@@ -32,89 +32,89 @@ let st = 0,pa = 0,sc = 0;
 stone.addEventListener("click", () => {
     timesPlayed++;
     iterations.innerText = ""+timesPlayed;
-    //boo.pause();
-    //claps.pause();
-    //ohno.pause();
+    boo.pause();
+    claps.pause();
+    ohno.pause();
     compVal=randomGen();
   if (compVal == 1) {
     result.innerText = "Draw, Computer chose Stone";
     info.style.backgroundColor = "blue";
     score_Draw++;
     drawScore.innerText = ""+score_Draw;
-    //ohno.play();
+    ohno.play();
   }
   if (compVal == 2) {
     result.innerText = "You Lost, Computer chose Paper";
     info.style.backgroundColor = "red";
     score_Comp++;
     compScore.innerText = ""+score_Comp;
-    //boo.play();
+    boo.play();
   }
   if (compVal == 3) {
     result.innerText = "You Win, Computer chose Scissor";
     info.style.backgroundColor = "green";
     score_Player++;
     playerScore.innerText = ""+score_Player;
-    //claps.play();
+    claps.play();
   }
 });
 
 paper.addEventListener("click", () => {
     timesPlayed++;
     iterations.innerText = ""+timesPlayed;
-    //boo.pause();
-    //claps.pause();
-    //ohno.pause();
+    boo.pause();
+    claps.pause();
+    ohno.pause();
     compVal=randomGen();
   if (compVal == 1) {
     result.innerText = "You Win, Computer chose Stone";
     info.style.backgroundColor = "green";
     score_Player++;
     playerScore.innerText = ""+score_Player;
-    //claps.play();
+    claps.play();
   }
   if (compVal == 2) {
     result.innerText = "Draw, Computer chose Paper";
     info.style.backgroundColor = "blue";
     score_Draw++;
     drawScore.innerText = ""+score_Draw;
-    //ohno.play();
+    ohno.play();
   }
   if (compVal == 3) {
     result.innerText = "You Loose, Computer chose Scissor";
     info.style.backgroundColor = "red";
     score_Comp++;
     compScore.innerText = ""+score_Comp;
-    //boo.play();
+    boo.play();
   }
 });
 
 scissor.addEventListener("click", () => {
     timesPlayed++;
     iterations.innerText = ""+timesPlayed;
-    //boo.pause();
-    //claps.pause();
-    //ohno.pause();
+    boo.pause();
+    claps.pause();
+    ohno.pause();
     compVal=randomGen();
   if (compVal == 1) {
     result.innerText = "You Loose, Computer chose Stone";
     info.style.backgroundColor = "red";
     score_Comp++;
     compScore.innerText = ""+score_Comp;
-    //boo.play();
+    boo.play();
   }
   if (compVal == 2) {
     result.innerText = "You Win, Computer chose Paper";
     info.style.backgroundColor = "green";
     score_Player++;
     playerScore.innerText = ""+score_Player;
-    //claps.play();
+    claps.play();
   }
   if (compVal == 3) {
     result.innerText = "Draw, Computer chose Scissor";
     info.style.backgroundColor = "blue";
     score_Draw++;
     drawScore.innerText = ""+score_Draw;
-    //ohno.play();
+    ohno.play();
   }
 });
